37a95d6fe3d587a3fb0012ba3292a53c49851230
Alina Pisarenko <pisarenko.alina@gmail.com>
2022-12-01 14:54:34 -0500

    commit tree d33108cefc0541533a0ed43c5d9c51f15b002456

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,17 @@
 class Pet < ActiveRecord::Base
+    belongs_to :shelter
+    belongs_to :adopter
+
+    def self.oldest
+        Pet.order(:age).last
+    end
+
+    def self.average_age 
+        Pet.average(:age).to_f
+    end
+
+    def in_dog_years
+        
+    end
 
 end
